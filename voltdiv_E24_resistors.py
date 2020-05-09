b94de51b75f0d2c57aa42ca8b753db0df096a646
Daniel Boulet <djboulet@gmail.com>
2020-05-09 08:04:38 -0400

    Removed E12 values for easier reading of chart.

--- voltdiv_E24_resistors.py
+++ voltdiv_E24_resistors.py
@@ -27,8 +27,7 @@ sys.path.insert(0, "..")
 pyx.text.set(mode="latex")  # allows use of latex commands in PyX
 
 resistors = [
-    1.0,	1.1,	1.2,    1.3,	1.5,	1.6,    1.8,	2.0,	2.2,    2.4,	2.7,	3.0,
-    3.3,	3.6,	3.9,    4.3,	4.7,	5.1,    5.6,	6.2,	6.8,    7.5,    8.2,    9.1
+    1.1,	1.3,	1.6,    2.0,	2.4,	3.0,	3.6,	4.3,	5.1,    6.2,	7.5,    9.1
 ]
 
 # Type 5 contour
