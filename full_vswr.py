cf912ddc3afd92bf181d8c4f18bfcfd50f83d1c7
Daniel Boulet <djboulet@gmail.com>
2021-12-15 12:50:11 -0500

    final version - text edits

--- full_vswr.py
+++ full_vswr.py
@@ -54,8 +54,9 @@ def dbw2watts(dbw):
 
 
 def cableloss(freq):
-    # cable loss per meter at freq
-    return (np.sqrt(freq) * 0.424232 + freq * 0.000563) / 100.0 / 0.3048
+    # cable loss in db per meter at freq
+	# source formula is db per 100 feet
+    return (np.sqrt(freq) * 0.356859 + freq * 0.000470) / 100.0 / 0.3048
 
 
 # measured return loss axes and block section
@@ -63,9 +64,7 @@ axis1_forward_power_meas_watts = {
     "tag": "axis14",
     "u_min": 10.0,
     "u_max": 150.0,
-    # "title_distance_center": -1.5,
     "title": r"$P_{fwd}$",
-    # "title_draw_center": True,
     "tick_levels": 5,
     "tick_text_levels": 4,
     "function": lambda u: watts2dbw(u),
@@ -84,9 +83,7 @@ axis2_reflected_power_meas_watts = {
     "tag": "axis25",
     "title": r"$P_{ref}$",
     "u_min": 0.1,
-    # "title_draw_center": True,
     "u_max": 10.0,
-    # "title_distance_center": 1.5,
     "function": lambda u: watts2dbw(u),
     "align_func": lambda u: watts2dbw(u),
     "tick_levels": 5,
@@ -103,11 +100,9 @@ block_reflected_watts = {
 
 axis3_return_loss_meas_vswr = {
     "tag": "axis36",
-    "u_min": 1.1,
-    "u_max": 5.0,
-    # "title_draw_center": True,
+    "u_min": 1.2,
+    "u_max": 3.0,
     "title": r"VSWR (Measured)",
-    # "title_distance_center": 1.9,
     "function": lambda u: vswr2rl(u),
     "align_func": lambda u: vswr2rl(u),
     "tick_levels": 5,
@@ -127,7 +122,6 @@ axis4_forward_power_meas_dbw = {
     "u_min": watts2dbw(10.0),
     "u_max": watts2dbw(100.0),
     "function": lambda u: -u,
-    # "title": r"Fwd (dbW)",
     "title_draw_center": True,
     "tick_side": "left",
     "tick_levels": 5,
@@ -141,7 +135,6 @@ axis5_reflected_power_meas_dbw = {
     "u_min": watts2dbw(0.1),
     "u_max": watts2dbw(7.0),
     "function": lambda u: u,
-    # "title": r"Ref (dbW)",
     "title_distance_center": -1.7,
     "title_draw_center": True,
     "tick_levels": 5,
@@ -155,7 +148,6 @@ axis6_return_loss_meas_dbw1 = {
     "u_max": 30.0,
     "function": lambda u: u,
     "title_distance_center": 1.5,
-    # "title": r"R.L. Meas (db)",
     "title_draw_center": True,
     "tick_levels": 5,
     "tick_text_levels": 4,
@@ -180,7 +172,6 @@ axis8_return_loss_true_dbw = {
     "u_max": 30.0,
     "function": lambda u: -u,
     "title_draw_center": True,
-    # "title": r"R.L. True (db)",
     "tick_levels": 5,
     "title_distance_center": 1.5,
     "tick_text_levels": 4,
@@ -189,11 +180,9 @@ axis8_return_loss_true_dbw = {
 
 axis10_return_loss_true_vswr = {
     "tag": "axis8-10",
-    "u_min": 1.1,
-    "u_max": 10.0,
-    # "title_draw_center": True,
+    "u_min": 1.2,
+    "u_max": 20.0,
     "title": r"VSWR (True)",
-    # "title_distance_center": 1.5,
     "function": lambda u: vswr2rl(u),
     "align_func": lambda u: vswr2rl(u),
     "tick_side": "left",
@@ -232,36 +221,30 @@ block_true_rl_dbw = {
 # cable loss block using type 5
 block_cable_loss = {
     "block_type": "type_5",
-    # 'wd_tag': 'ratio',
-    # 'mirror_y':True,
+
     "u_func": lambda u: u,
-    "v_func": lambda x, v: x / cableloss(v),
-    # lengths
-    "u_values": list(np.linspace(10.0, 40.0, 11)),
+    "u_values": list(np.linspace(10.0, 40.0, 16)),
     "u_scale_type": "manual point",
 	'u_title_distance_center':1.0,
-    # 'u_manual_axis_data': {12.0: '7', 14.0: '6', 16.0: '5', 18.0: '4', 21.0: '3', 24.0: '2', 28.0: '1'},
     "u_title": "Cable length (m)",
-    # teeth on front derailer
+
+    "v_func": lambda x, v: x / cableloss(v),
     "v_values": [1.75, 3.5, 7.0, 14.0, 28.0, 50.0, 144.0, 440.0],
-    # 'v_scale_type': 'manual point',
-    # 'v_manual_axis_data': {28.0: 'Small', 38.0: 'Medium', 48.0: 'Large'},
     "v_title": "Freq (MHz)",
+
     "wd_tick_levels": 4,
     "wd_tick_text_levels": 1,
     "wd_tick_side": "left",
     "wd_title": r"LMR195\textsuperscript{\textregistered} Cable Loss (db)",
     "wd_title_opposite_tick": True,
     "wd_tag": "axis9",
-    # 'wd_scale_type':'manual',
     "isopleth_values": [[28, 50, "x"]],
 }
 
 main_params = {
-    "filename": "full_vswr.pdf",
+    "filename": outputfile,
     "paper_height": 8.0 * 2.54,
     "paper_width": 10.5 * 2.54,
-    # "block_params": [main_block],
     "block_params": [
         block_measured_rl_dbw,
         block_true_rl_dbw,
@@ -274,31 +257,32 @@ main_params = {
     "transformations": [("rotate", 0.01), ("scale paper",)],
     "title_str": r"\huge \textbf{True VSWR as a result of cable attenuation} \
 		\par\medskip \normalsize \copyright Daniel Boulet (2021)",
-    "title_x": 5.0,
-    "title_y": 20.0,
+    "title_x": 7.0,
+    "title_y": 19.0,
     "title_box_width": 10.0,
     "extra_texts": [
         {
-            "x": -1.0,
-            "y": 5.0,
-            "text": r"\noindent \textbf{How to use:} \
-				\par \medskip \noindent Calculate measured VSWR by drawing \
-				line 'A' from forward power axis through the reflected \
+            "x": 2.0,
+            "y": 16.5,
+            "text": r"\noindent Calculate true VSWR by drawing \
+				a straight line from forward power axis through the reflected \
 				power axis to the measured VSWR axis. \
-				To compensate for cable loss draw straight line 'B' from \
-				measured VSWR to cable loss value.  Compensated VSWR can be read at \
-				intersection of VWSR and line 'B'. \
-				\par \medskip \
-				\par \medskip \noindent \textbf{Example:} \
-				\par \noindent Measured forward and reflected power are \
-				60W 5W respectively thus measured VSWR is approximately $1.81:1$. \
-				However 28m of LMR195\textsuperscript{\textregistered} will attenuate a 50Mhz signal by \
-				approximately 2.8db. Therefore true \
-				VSWR is approximately $3.4:1$.",
+				To compensate for cable loss draw straight line from \
+				measured VSWR to cable loss value.  True VSWR can be read at \
+				the intersection of true VWSR axis.",
             "width": 10.0,
         },
+        {
+            "x": 0.0,
+            "y": 3.0,
+            "text": r"\noindent \textbf{Example:} \
+				\par \noindent Measured forward and reflected power are \
+				60W and 5W respectively thus measured VSWR is 1.81:1. \
+				However 28m of LMR195\textsuperscript{\textregistered} will attenuate \
+				a 50Mhz signal by 2.34 dB therefore true VSWR is 2.96:1.",
+            "width": 9.0,
+        },
     ],
-    #     "debug": False,
     # "make_grid": True,
 }
 Nomographer(main_params)
